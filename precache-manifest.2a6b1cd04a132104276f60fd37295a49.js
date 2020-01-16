b877ded887b5a45ff1ed081d7a1c4a86826acefe
Dabbyd123 <dbrocker89@gmail.com>
2020-01-16 15:14:12 -0600

    Updates

---
+++ precache-manifest.2a6b1cd04a132104276f60fd37295a49.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373aabfd7a05876d9b43a71eefc9be11",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f58343f534c630d95181",
+    "url": "/portfolio/static/css/2.d35c4295.chunk.css"
+  },
+  {
+    "revision": "e97d9fbe41c1504fc5f9",
+    "url": "/portfolio/static/css/main.e422ddca.chunk.css"
+  },
+  {
+    "revision": "f58343f534c630d95181",
+    "url": "/portfolio/static/js/2.e00d1ad3.chunk.js"
+  },
+  {
+    "revision": "e97d9fbe41c1504fc5f9",
+    "url": "/portfolio/static/js/main.d3eccbe9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "515f2c5de9e1b607a090409e4f7fef61",
+    "url": "/portfolio/static/media/city-ConvertImage.515f2c5d.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "15845cc67e5f5f2db6b896f11a9107d7",
+    "url": "/portfolio/static/media/mars-rovers.15845cc6.jpg"
+  },
+  {
+    "revision": "79069651ea65224acb82b5890d3f8f5b",
+    "url": "/portfolio/static/media/me.79069651.png"
+  },
+  {
+    "revision": "4fdee302b7d42ced6899d93122dbf881",
+    "url": "/portfolio/static/media/robofriends.4fdee302.jpg"
+  },
+  {
+    "revision": "83c2e20482727fdd16468fa00f7609ef",
+    "url": "/portfolio/static/media/todoList.83c2e204.jpeg"
+  }
+]);
